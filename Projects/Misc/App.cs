78433a76228be19ac232b518195ec224d90f6745
Luke McMahon <lukemcmahon96@gmail.com>
2017-02-17 00:51:26 +1100

    + Uncommented main event, because im an idiot and committed while it was commented... + Removed redundant using statements

--- Projects/Misc/App.cs
+++ Projects/Misc/App.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Misc.Files.PDF_Generator;
 using Misc.Text;
 
 namespace Misc
@@ -13,7 +12,7 @@ namespace Misc
 
         private static void Main()
         {
-           // Run();
+            Run();
            
 
             Console.Read();
